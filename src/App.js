dca37a18130b59f87e6fb70fe851c6338efb84fa
David Proskin <proskd@gmail.com>
2019-05-06 15:34:01 -0400

    add an error handler when hitting the image endpoint

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
     }).then(res => res.json())
     .then((count) => {
       this.setState(Object.assign(this.state.user, {entries: count}))
-    })
+    }).catch(error => console.log(error));
   }
 
   loadUser = (data) => {
