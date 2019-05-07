4a0ac3928e0d61fdb16c89dad96ed71b5105b63a
David Proskin <proskd@gmail.com>
2019-05-07 06:50:12 -0400

    remove unneeded console.log

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 const getApiHost = (env) => {
-    console.log("fetching config for env " + env);
     if (env === 'prod') {
         return 'https://smart-brain-api-proskd.herokuapp.com';
     } else if (env === 'dev') {
